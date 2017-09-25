d43a5c64fde1d6ebe00684b9e6d70a4137d5de8c
Glenn Hermans <glhermans@gmail.com>
2017-09-25 20:02:46 +0200

    Update locations.lua

--- ext/locations.lua
+++ ext/locations.lua
@@ -29,6 +29,9 @@ local locations = {
   {name="Police station", id=60, x=818.221, y=-1289.883, z=26.300},
   {name="Prison", id=285, x=1679.049, y=2513.711, z=45.565},
   {name="Yacht", id=410, x=-2045.800, y=-1031.200, z=11.900},
+  {name="Weed Warehouse", id=496, x=1308.95, y=4362.15, z=30.0},
+  {name="Cocaine Warehouse", id=497, x=387.40, y=3583.45, z=30.0},
+  {name="Meth Warehouse", id=499, x=1179.732, y=-3114.203, z=6.028}  
 }
 
 Citizen.CreateThread(function()
