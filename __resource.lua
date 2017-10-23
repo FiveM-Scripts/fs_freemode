6819d1455489edeae17775864ba47404daaf5972
ghermans <glhermans@gmail.com>
2017-10-23 21:27:21 +0200

    Updated manifest version

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_type 'gametype' { name = 'fs_freemode'}
 
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 dependency 'essentialmode'
 
