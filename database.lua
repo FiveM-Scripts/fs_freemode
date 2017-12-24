0107cad36296a509fab8d4550e9928c5025ff737
ghermans <glhermans@gmail.com>
2017-12-24 03:02:59 +0100

    prepare v1.3.1

--- database.lua
+++
@@ -1,6 +0,0 @@
-database = {
-    driver = GetConvar('database_driver', 'mysql-async'),
-    host = GetConvar('es_couchdb_url', '127.0.0.1'),
-    port = GetConvar('es_couchdb_port', '5984'),
-    username = GetConvar('es_couchdb_password', 'admin:changeme')
-}
